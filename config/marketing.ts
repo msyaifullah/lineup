fa03ec5498e0e19580f713420e92dfaa552886de
Masykuri Syaifullah <msyaifullah@gmail.com>
2024-11-04 12:16:48 +0700

    remove unused code

--- config/marketing.ts
+++ config/marketing.ts
@@ -1,11 +1,7 @@
 import { MarketingConfig } from "types"
 
 export const marketingConfig: MarketingConfig = {
-  mainNav: [
-    {
-      title: "Features",
-      href: "/#features",
-    },
+  mainNav: [    
     {
       title: "Products",
       href: "/products",
