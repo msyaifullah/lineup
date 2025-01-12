59aa114e38ed70a7ab33fc8f40b4206861d40cdb
Masykuri Syaifullah <msyaifullah@gmail.com>
2025-01-12 13:34:07 +0700

    add contract

--- app/(marketing)/nft-staking/config/contracts.ts
+++ app/(marketing)/nft-staking/config/contracts.ts
@@ -1,27 +1,28 @@
-import { chain } from "./chain";
-import { client } from "./client";
-import { getContract } from "thirdweb";
-import { stakingABI } from "./stakingABI";
+import { getContract } from "thirdweb"
 
-const nftContractAddress = "0xe2e1df5e5E962bA13B09467af15f13bd29f029F9";
-export const rewardTokenContractAddress = "0xb9Ccb2674d07170B704F25643c3AAE8DAefc49A2";
-const stakingContractAddress = "0xFB66d02f9a6202430c6D9f4D502e050fB05a6E4F";
+import { chain } from "./chain"
+import { client } from "./client"
+import { stakingABI } from "./stakingABI"
+
+export const nftContractAddress = "0xe2e1df5e5E962bA13B09467af15f13bd29f029F9"
+export const rewardTokenContractAddress = "0xb9Ccb2674d07170B704F25643c3AAE8DAefc49A2"
+export const stakingContractAddress = "0xFB66d02f9a6202430c6D9f4D502e050fB05a6E4F"
 
 export const NFT_CONTRACT = getContract({
-    client: client,
-    chain: chain,
-    address: nftContractAddress
-});
+  client: client,
+  chain: chain,
+  address: nftContractAddress,
+})
 
 export const REWARD_TOKEN_CONTRACT = getContract({
-    client: client,
-    chain: chain,
-    address: rewardTokenContractAddress
-});
+  client: client,
+  chain: chain,
+  address: rewardTokenContractAddress,
+})
 
 export const STAKING_CONTRACT = getContract({
-    client: client,
-    chain: chain,
-    address: stakingContractAddress,
-    abi: stakingABI
-});
+  client: client,
+  chain: chain,
+  address: stakingContractAddress,
+  abi: stakingABI,
+})
